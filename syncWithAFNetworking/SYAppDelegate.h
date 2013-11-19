aa37b584fd936d20a4e9dc5b1bdea8d61553c394
Lagspoon <lagspoon@gmail.com>
2013-11-19 09:47:18 +0100

    Initial Commit

---
+++ syncWithAFNetworking/SYAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SYAppDelegate.h
+//  syncWithAFNetworking
+//
+//  Created by Olivier Delecueillerie on 19/11/2013.
+//  Copyright (c) 2013 Olivier Delecueillerie. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SYAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ syncWithAFNetworking/SYViewController.h
@@ -0,0 +1,13 @@
+//
+//  SYViewController.h
+//  syncWithAFNetworking
+//
+//  Created by Olivier Delecueillerie on 19/11/2013.
+//  Copyright (c) 2013 Olivier Delecueillerie. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SYViewController : UIViewController
+
+@end
